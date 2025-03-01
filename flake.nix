57aea566494fa6f88e7ea7832b9a886389829096
Michael Jossen <michael.jossen@rwth-aachen.de>
2025-03-01 12:12:34 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Rust development flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+      in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [rustToolchain
+            
+                fontconfig
+
+    vulkan-headers
+    vulkan-loader
+            ];
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+              libGL
+              libxkbcommon
+              wayland
+              xorg.libX11
+              xorg.libXcursor
+              xorg.libXi
+              xorg.libXrandr
+            ]);
+          };
+
+        }
+    );
+}
